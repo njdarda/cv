8a7af79096669855c9d647688f155e07e8505136
Jedrzej Darda <jedrekdarda@gmail.com>
2021-09-11 18:15:40 +0200

    add basic structure

--- src/app.vue
+++ src/app.vue
@@ -1,19 +1,39 @@
 <template lang="pug">
 #app
     img(alt='Vue logo', src='./assets/logo.png')
-    HelloWorldComponent(msg='Welcome to Your Vue.js + TypeScript App')
+    HelloWorldComponent(ref='HelloWorldComponent', msg='Welcome to Your Vue.js + TypeScript App')
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+// Components
 import HelloWorldComponent from './components/hello-world.vue'
+// Types
+import { TestType } from './types/types'
+// Libraries and Helpers
 
 @Component({
     components: {
         HelloWorldComponent,
     },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+    $refs!: {
+        HelloWorldComponent: HelloWorldComponent
+    }
+
+    testProp!: TestType
+
+    // Hooks
+    created(): void {
+        this.helloWorld()
+    }
+
+    // Methods
+    helloWorld(): void {
+        console.log('Hello World!')
+    }
+}
 </script>
 
 <style lang="scss">
--- src/declarations/vue-types.d.ts
+++ src/declarations/vue-types.d.ts
@@ -3,19 +3,14 @@ import Vue from 'vue'
 
 declare module 'vue/types/vue' {
     interface Vue {
-        $socket: WebSocket
+        $testGlobalProp: ?string
     }
 }
 
 declare module 'vue/types/options' {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface ComponentOptions<V extends Vue> {
-        sockets?: {
-            onmessage: (event: MessageEvent) => boolean
-            onclose: (event: MessageEvent) => boolean
-            onerror: (event: MessageEvent) => boolean
-            onopen: (event: MessageEvent) => boolean
-        }
+        testOption?: ?string
     }
 }
 
