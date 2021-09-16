15f0fb10e798f4f3a6be942968c515a93537df55
Jedrzej Darda <jedrekdarda@gmail.com>
2021-09-16 16:33:17 +0200

    add header module

---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.header-component
+    img.photo(:alt='header.name', :src='require("@/assets/" + header.photo)')
+    .header-bar
+        h1.name {{ header.name }}
+        h2.mb-2(v-for='link in header.links')
+            a(:href='link.url + link.path')
+                b-icon(:icon='link.type')
+                |
+                | {{ link.path }}
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class HelloWorld extends Vue {
+    @Prop() header!: Record<string, unknown>
+}
+</script>
