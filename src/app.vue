9aa39da885a3907fcad568cd33e108472a91c67e
Jedrzej Darda <jedrekdarda@gmail.com>
2021-09-17 14:25:26 +0200

    merge left and right column component into one

--- src/app.vue
+++ src/app.vue
@@ -5,9 +5,9 @@
         .header
             HeaderComponent(:header='cvData.header')
         .left-column
-            LeftColumnComponent(:sections='cvData.leftColumn', :contacts='cvData.contacts')
+            ColumnComponent(:sections='cvData.leftColumn', :contacts='cvData.contacts')
         .right-column
-            RightColumnComponent(:sections='cvData.rightColumn')
+            ColumnComponent(:sections='cvData.rightColumn')
         footer#footer Footer
     .background
 </template>
@@ -16,27 +16,23 @@
 import { Vue, Component } from 'vue-property-decorator'
 // Components
 import HeaderComponent from '@/components/header.vue'
-import LeftColumnComponent from '@/components/left-column.vue'
-import RightColumnComponent from '@/components/right-column.vue'
+import ColumnComponent from '@/components/column.vue'
 import CssVariablesComponent from '@/components/css-variables.vue'
 // Types
 // Libraries and Helpers
 // Data
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import jwData from '@/data/jw.json'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import njdData from '@/data/njd.json'
 
 @Component({
     components: {
         HeaderComponent,
-        LeftColumnComponent,
-        RightColumnComponent,
+        ColumnComponent,
         CssVariablesComponent,
     },
 })
 export default class App extends Vue {
-    cvData = jwData
+    cvData = njdData
+
     cssVariables = {
         'theme-color': this.cvData.themeColor,
         photo: `url('${require('@/assets/' + this.cvData.header.photo)}')`,
