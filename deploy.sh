c63f18b255495df10b32bd9da45e0f84220aefd2
Jedrzej Darda <jedrekdarda@gmail.com>
2021-09-18 22:07:14 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,15 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
+cd "$(dirname "$0")"
+rm -rf ./docs
+mv ./dist ./docs
+git add .
+git commit -m 'deploy'
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:njdarda/cv.git main:gh-pages
+# git push
 
 cd -
