762102481edde39c1b4a304e86b0a412b153001a
Jedrzej Darda <jedrekdarda@gmail.com>
2021-09-19 21:35:21 +0200

    add pug and enforce formatting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+<template lang="pug">
+.home
+    img(alt='Vue logo', src='../assets/logo.png')
+    HelloWorld(msg='Welcome to Your Vue.js + TypeScript App').
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+    components: {
+        HelloWorld,
+    },
 })
 export default class Home extends Vue {}
 </script>
