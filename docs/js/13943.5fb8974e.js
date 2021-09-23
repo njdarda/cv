1824221509f2368997f5eb0d7cc210546db224f8
Jedrzej Darda <jedrekdarda@gmail.com>
2021-09-23 16:42:17 +0200

    deploy

---
+++ docs/js/13943.5fb8974e.js
@@ -0,0 +1,1 @@
+(self["webpackChunkcv"]=self["webpackChunkcv"]||[]).push([[13943],{13943:function(l){l.exports='<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16"><path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"></path></svg>'}}]);
