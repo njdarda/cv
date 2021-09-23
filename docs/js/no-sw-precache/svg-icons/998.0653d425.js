eca4ef7325c9a6f4f2d187e7f809c388359a03b5
Jedrzej Darda <jedrekdarda@gmail.com>
2021-09-24 00:53:32 +0200

    deploy

---
+++ docs/js/no-sw-precache/svg-icons/998.0653d425.js
@@ -0,0 +1,1 @@
+(self["webpackChunkcv"]=self["webpackChunkcv"]||[]).push([[68472],{86143:function(s){s.exports='<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-pentagon-fill" viewBox="0 0 16 16"><path d="m8 0 8 6.5-3 9.5H3L0 6.5 8 0z"></path></svg>'}}]);
